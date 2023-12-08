951f41266fd4e8be7f6addb8e287551c00972b90
Jeffimenson <jeffengim@gmail.com>
2023-12-07 22:40:12 -0500

    Finish base of website needed for grade

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+
+const body = document.querySelector('body');
+const prevBut = body.querySelector('#prev');
+const nextBut = body.querySelector('#next');
+const slider = body.querySelector('.slider');
+
+const maxSlide = 200;
+const minSlide = 0;
+
+let currentSlide = 0;
+nextBut.addEventListener('click', () => {
+  if (currentSlide < maxSlide) {
+    currentSlide += 100;
+    slider.setAttribute('style', `right: ${currentSlide}%;`);
+  }
+});
+
+prevBut.addEventListener('click', () => {
+  if (currentSlide > minSlide) {
+    currentSlide -= 100;
+    slider.setAttribute('style', `right: ${currentSlide}%;`);
+  }
+});
